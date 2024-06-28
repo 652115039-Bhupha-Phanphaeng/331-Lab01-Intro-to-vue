3368a355d67cf8c82a83714a5a31ce72babfb0c9
Bhupha Phanphaeng <bhupha_p@cmu.ac.th>
2024-06-28 09:36:57 +0700

    4.5

--- main.js
+++ main.js
@@ -6,13 +6,15 @@ createApp({
         const description = ref('Warm and cozy boots')
         const image = ref('./assets/images/socks_green.jpg')
         const link = ref('https://www.camt.cmu.ac.th/')
-        const inStock = ref(false)
+        const inStock = ref(true)
+        const inventory = ref(100)
         return {
             product,
             description,
             image,
             link,
-            inStock
+            inStock,
+            inventory
         }
     }
 }).mount('#app')
