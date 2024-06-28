4ae4c78d5f4409db2e3c98644b02ffcd702b276c
Bhupha Phanphaeng <bhupha_p@cmu.ac.th>
2024-06-28 14:00:22 +0700

    9.1

--- main.js
+++ main.js
@@ -17,7 +17,7 @@ createApp({
         ])
         const variants = ref([
             { id: 2234, color: 'green', image: './assets/images/socks_green.jpg', quantity: 50, onSale: true},
-            { id: 2235, color: 'blue', image: './assets/images/socks_blue.jpg', quantity: 0, onSale: false},
+            { id: 2235, color: 'blue', image: './assets/images/socks_blue.jpg', quantity: 0, onSale: false}
         ])
         const selectedVariant = ref(0)
         const cart = ref(0)
