9b327b832bf47821c3ddc5f07d00b5f2770d35ca
Bhupha Phanphaeng <bhupha_p@cmu.ac.th>
2024-06-28 14:39:10 +0700

    9.6 (fix)

--- components/ProductDisplay.js
+++ components/ProductDisplay.js
@@ -4,49 +4,50 @@ const productDisplay = {
     /*html*/
     `
     <div class="product-display">
-        <div class="product-container">
-            <div class="product-image">
-                <img :src="image">
+            <div class="product-container">
+                <div class="product-image">
+                    <img :src="image" :class="{OutOfStockImg: !inStock}">
+                </div>
             </div>
         </div>
         <div class="product-info">
-            <h1>{{ title }}</h1>
+            <a :href="link" target="_blank"><h1>{{title}}</h1></a>
+            <p>{{description}}</p>
             <p v-if="inventory > 10">In Stock</p>
             <p v-else-if="inventory <= 10 && inventory > 0">Almost out of Stock</p>
             <p v-else>Out of Stock</p>
+            <p v-if="onSale">{{title+" is on sale"}}</p>
             <ul>
-                <li v-for="detail in details">{{ detail }}</li>
+                <li v-for="detail in details">{{detail}}</li>
             </ul>
-            <div
-                v-for="(variant, index) in variants"
-                :key="variant.id"
-                @mouseover="updateVariant(index)"
-                class="color-circle"
-                :style="{ backgroundColor: variant.color }"
-            ></div>
-            <button
-                @click="addToCart"
-                :disabled="!inStock"
-                :class="{ disabledButton: !inStock }"
-            >Add to Cart</button>
+            <span v-for="size in sizes">{{size+" "}}</span>
+            <div v-for="(variant, index) in variants" :key="variant.id" @mouseover="updateVariant(index)" class="color-circle" :style="{backgroundColor: variant.color}">
+                {{variant.color}}
+            </div>
+            <p v-if="inStock">In Stock</p>
+            <p v-else>Out of Stock</p>
+            <button class="button" :disabled="!inStock" @click="addToCart" :class="{disabledButton: !inStock}">Add To Cart</button>
+            <button class="button" @click="toggleStock">Toggle Stock</button>
         </div>
     </div>
-
     `,
     setup() {
         const product = ref('Boots')
         const brand = ref('SE 331')
-        // const image = ref('../assets/images/socks_green.jpg')
-        // const inStock = ref(true)
+        const description = ref('Warm and cozy boots')
+        const link = ref('https://www.camt.cmu.ac.th/')
         const inventory = ref(100)
         const details = ref([
             '50% cotton',
             '30% wool',
             '20% polyester'
         ])
+        const sizes = ref([
+            'S', 'M', 'L'
+        ])
         const variants = ref([
-            { id: 2234, color: 'green', image: './assets/images/socks_green.jpg', quantity: 50},
-            { id: 2235, color: 'blue', image: './assets/images/socks_blue.jpg', quantity: 0}
+            { id: 2234, color: 'green', image: './assets/images/socks_green.jpg', quantity: 50, onSale: true},
+            { id: 2235, color: 'blue', image: './assets/images/socks_blue.jpg', quantity: 0, onSale: false}
         ])
         const selectedVariant = ref(0)
         const cart = ref(0)
@@ -57,6 +58,9 @@ const productDisplay = {
         }
         function updateImage(variantImage) {
             image.value = variantImage
+        }
+        function toggleStock() {
+            inStock.value = !inStock.value
         }
         function updateVariant(index) {
             selectedVariant.value = index;
@@ -72,16 +76,24 @@ const productDisplay = {
         const inStock = computed(() => {
             return variants.value[selectedVariant.value].quantity
         })
+        const onSale = computed(() => {
+            return variants.value[selectedVariant.value].onSale
+        })
         return {
             title,
+            description,
             image,
+            link,
             inStock,
             inventory,
+            onSale,
             details,
+            sizes,
             variants,
             cart,
             addToCart,
             updateImage,
+            toggleStock,
             updateVariant
         }
     }
--- main.js
+++ main.js
@@ -2,69 +2,9 @@ const { createApp, ref, computed } = Vue
 
 const app = createApp({
     setup() {
-        const product = ref('Boots')
-        const brand = ref('SE 331')
-        const description = ref('Warm and cozy boots')
-        const link = ref('https://www.camt.cmu.ac.th/')
-        const inventory = ref(100)
-        const details = ref([
-            '50% cotton',
-            '30% wool',
-            '20% polyester'
-        ])
-        const sizes = ref([
-            'S', 'M', 'L'
-        ])
-        const variants = ref([
-            { id: 2234, color: 'green', image: './assets/images/socks_green.jpg', quantity: 50, onSale: true},
-            { id: 2235, color: 'blue', image: './assets/images/socks_blue.jpg', quantity: 0, onSale: false}
-        ])
-        const selectedVariant = ref(0)
         const cart = ref(0)
-
-
-        function addToCart() {
-            cart.value += 1
-        }
-        function updateImage(variantImage) {
-            image.value = variantImage
-        }
-        function toggleStock() {
-            inStock.value = !inStock.value
-        }
-        function updateVariant(index) {
-            selectedVariant.value = index;
-        }
-
-
-        const title = computed(() => {
-            return brand.value + ' ' + product.value
-        })
-        const image = computed(() => {
-            return variants.value[selectedVariant.value].image
-        })
-        const inStock = computed(() => {
-            return variants.value[selectedVariant.value].quantity
-        })
-        const onSale = computed(() => {
-            return variants.value[selectedVariant.value].onSale
-        })
         return {
-            title,
-            description,
-            image,
-            link,
-            inStock,
-            inventory,
-            onSale,
-            details,
-            sizes,
-            variants,
-            cart,
-            addToCart,
-            updateImage,
-            toggleStock,
-            updateVariant
+            cart
         }
     }
 })
