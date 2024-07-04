6c9a2adcbf41bd67787b1f1693d233c585b666a8
Bhupha Phanphaeng <bhupha_p@cmu.ac.th>
2024-07-05 04:30:48 +0700

    11.8

--- components/ReviewForm.js
+++ components/ReviewForm.js
@@ -31,6 +31,10 @@ const reviewForm = {
         })
 
         function onSubmit() {
+            if (form.name === '' || form.review === '' || form.rating === null) {
+                alert('Review is incomplete. Please fill out every field.')
+                return
+            }
             const productReview = {
                 name: form.name,
                 review: form.review,
