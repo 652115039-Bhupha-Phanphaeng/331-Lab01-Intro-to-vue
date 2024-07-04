3dcd5a59c65001cadbda8372a30d6f3d6cc0127d
Bhupha Phanphaeng <bhupha_p@cmu.ac.th>
2024-07-05 03:56:35 +0700

    11.3

--- components/ReviewForm.js
+++ components/ReviewForm.js
@@ -1,17 +1,18 @@
 const reviewForm = {
+    
     template: 
     /*html*/
     `
         <form class="review-form">
             <h3>Leave a review</h3>
             <label for="name">Name:</label>
-            <input id="name">
+            <input id="name" v-model="form.name">
 
             <label for="review">Review:</label>
-            <textarea id="review"></textarea>
+            <textarea id="review" v-model="form.review"></textarea>
 
             <label for="rating">Rating:</label>
-            <select id="rating">
+            <select id="rating" v-model="form.rating">
                 <option>5</option>
                 <option>4</option>
                 <option>3</option>
